91617bdc2589417f8d045690cd1acaeb331ca722
davidmorra <motogge@gmail.com>
2021-12-18 23:38:01 +0400

    Implemented delegate pattern to get data from AddTaskViewController to main view controller and append to array.

--- ToDo/Controllers/AddTaskViewController.swift
+++ ToDo/Controllers/AddTaskViewController.swift
@@ -7,34 +7,59 @@
 
 import UIKit
 
-class AddTaskViewController: UITableViewController {
+protocol AddTaskViewControllerDelegate: AnyObject {
+    func addTaskViewControllerDidCancel(_ controller: AddTaskViewController)
+    func addTaskViewController(_ controller: AddTaskViewController, didFinishAdding item: ToDoListItem)
+}
+
+class AddTaskViewController: UITableViewController, UITextFieldDelegate {
+
+    weak var delegate: AddTaskViewControllerDelegate?
     
+    // MARK: - View Controller methods
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+        textFieldOutlet.delegate = self
+        
         setUpNavigation()
         
         textFieldOutlet.addTarget(self, action: #selector(doneButtonTapped), for: .editingDidEndOnExit)
+        textFieldOutlet.enablesReturnKeyAutomatically = true
+
     }
-    
-    // MARK: - Outlets
-    @IBOutlet weak var textFieldOutlet: UITextField!
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if self.isMovingFromParent {
+            delegate?.addTaskViewControllerDidCancel(self)
+        }
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationItem.largeTitleDisplayMode = .never
         textFieldOutlet.becomeFirstResponder()
     }
+
     
+    
+    // MARK: - Outlets
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    
+    
+    // MARK: - Setup work
     func setUpNavigation() {
-        title = "Add a task"
 
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneButtonTapped))
     }
     
-    @objc func doneButtonTapped() {
-        print("text field: \(textFieldOutlet.text!)")
-        navigationController?.popViewController(animated: true)
-    }
     
+    @objc func doneButtonTapped() {
+        let item = ToDoListItem()
+        item.text = textFieldOutlet.text!
+        
+        delegate?.addTaskViewController(self, didFinishAdding: item)
+        
+//        navigationController?.popViewController(animated: true)
+    }
 }
--- ToDo/Controllers/ToDoViewController.swift
+++ ToDo/Controllers/ToDoViewController.swift
@@ -7,33 +7,24 @@
 
 import UIKit
 
-class ToDoViewController: UITableViewController {
+class ToDoViewController: UITableViewController, AddTaskViewControllerDelegate {
+    
     var items = [ToDoListItem]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setUpNavigation()
         
         let item1 = ToDoListItem()
         item1.text = "Hello"; item1.isDone = true
         items.append(item1)
         
         let item2 = ToDoListItem()
-        item2.text = "World"
+        item2.text = "Hello"; item2.isDone = false
+
         items.append(item2)
         
-        let item3 = ToDoListItem()
-        item3.text = "Clean room"
-        items.append(item3)
-        
-        let item4 = ToDoListItem()
-        item4.text = "Walk the dog"; item4.isDone = true
-        items.append(item4)
-        
-        let item5 = ToDoListItem()
-        item5.text = "Cook"; item5.isDone = true
-        items.append(item5)
+        setUpNavigation()
         
     }
 
@@ -42,21 +33,21 @@ class ToDoViewController: UITableViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.largeTitleDisplayMode = .always
     }
-
+    
+    //MARK: - Setup work
     func setUpNavigation() {
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(addToTableView))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(addButtonTapped))
     }
     
-    @objc func addToTableView() {
-//        let addToDoVC = AddTaskViewController()
+    @objc func addButtonTapped() {
         let addTaskViewController = storyboard?.instantiateViewController(withIdentifier: "AddTaskViewController") as! AddTaskViewController
-        
+        addTaskViewController.delegate = self
+        addTaskViewController.title = "Add a task"
         
         navigationController?.pushViewController(addTaskViewController, animated: true)
     }
     
     // MARK: - Table view data source
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
@@ -64,27 +55,41 @@ class ToDoViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "toDoCell", for: indexPath)
         
-        let label = cell.viewWithTag(1000) as! UILabel
+        cell.accessoryType = .disclosureIndicator
         
+        // cell text
+        let label = cell.viewWithTag(1000) as! UILabel
         label.text = items[indexPath.row].text
-        cell.accessoryType = items[indexPath.row].isDone ? .checkmark : .none
+
+        // checkmark image
+        let chekImage = items[indexPath.row].isDone ? "checkmark.circle.fill" : "circle"
+        let checkmarkImage = cell.viewWithTag(1001) as! UIImageView
+        checkmarkImage.image = UIImage(systemName: chekImage)
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if let cell = tableView.cellForRow(at: indexPath) {
-            items[indexPath.row].isDone.toggle()
-            if items[indexPath.row].isDone {
-                cell.accessoryType = .checkmark
-            } else {
-                cell.accessoryType = .none
-            }
-        }
+        let item = items[indexPath.row]
+        item.isDone.toggle()
         
+        tableView.reloadRows(at: [indexPath], with: .automatic)
         tableView.deselectRow(at: indexPath, animated: true)
+        
     }
+    
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        let addTaskViewController = storyboard?.instantiateViewController(withIdentifier: "AddTaskViewController") as! AddTaskViewController
+        addTaskViewController.title = "Edit Item"
+        addTaskViewController.delegate = self
+        addTaskViewController.textFieldOutlet?.text = "\(items[indexPath.row].text)"
+        
+        print(items[indexPath.row].text)
+        
+        navigationController?.pushViewController(addTaskViewController, animated: true)
+    }
+    
     
     //MARK: - Swipe-to-Delete
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -93,4 +98,18 @@ class ToDoViewController: UITableViewController {
         tableView.deleteRows(at: [indexPath], with: .automatic)
     }
 
+    //MARK: - Protocol methods
+    func addTaskViewControllerDidCancel(_ controller: AddTaskViewController) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func addTaskViewController(_ controller: AddTaskViewController, didFinishAdding item: ToDoListItem) {
+        let index = items.count
+        let indexPath = IndexPath(row: index, section: 0)
+        
+        items.append(item)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+        
+        navigationController?.popViewController(animated: true)
+    }
 }
